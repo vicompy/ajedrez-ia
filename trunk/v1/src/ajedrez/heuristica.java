81f6452f0c621d689e92bf2f20544e8b959852ac
williambl17 <williambl17@85f47791-3065-007b-8d44-3d2c4d769e46>
2011-05-26 03:48:22 +0000

    Metodo validaPeon desarrollado

--- trunk/v1/src/ajedrez/heuristica.java
+++ trunk/v1/src/ajedrez/heuristica.java
@@ -11,13 +11,62 @@ package ajedrez;
  */
 public class heuristica {
 
-    public boolean validaPeon(int oX, int oY, int dX, int dY){
+    public boolean validaPeon(int oX, int oY, int dX, int dY, int[][] tablero){
         boolean resultado = false;
+        int pieza = tablero[oX][oY];
+        if(pieza > 0)
+        {
+            //es una pieza blanca
+            if(dX == oX - 1 && dY == oY && tablero[dX][dY] == 10){
+                //se mueve una casilla hacia arriba si esta vacia
+                resultado = true;
+            }
+            else if(dX == oX - 1 && dY == oY - 1 && tablero[dX][dY] < 0){
+                //se mueve en diagonal a la izquierda si hay una pieza del oponente
+                resultado = true;
+            }
+            else if(dX == oX - 1 && dY == oY + 1 && tablero[dX][dY] < 0){
+                //se mueve en diagonal a la derecha si hay una pieza del oponente
+                resultado = true;
+            }
+        }
+        else
+        {
+            //es una pieza negra
+            if(dX == oX + 1 && dY == oY && tablero[dX][dY] == 10){
+                //se mueve una casilla hacia abajo si esta vacia
+                resultado = true;
+            }
+            else if(dX == oX + 1 && dY == oY - 1 && tablero[dX][dY] > 0){
+                //se mueve en diagonal a la izquierda si hay una pieza del oponente
+                resultado = true;
+            }
+            else if(dX == oX + 1 && dY == oY + 1 && tablero[dX][dY] > 0){
+                //se mueve en diagonal a la derecha si hay una pieza del oponente
+                resultado = true;
+            }
+        }
         return resultado;
     }
 
-    public boolean validaTorre(){
+    public boolean validaTorre(int oX, int oY, int dX, int dY, int pieza){
         boolean resultado = false;
+        if(pieza > 0)
+        {
+            //es una pieza blanca
+            if(dX == oX - 1 && dY == oY){
+                //se mueve una casilla hacia arriba
+                resultado = true;
+            }
+        }
+        else
+        {
+            //es una pieza negra
+            if(dX == oX + 1 && dY == oY){
+                //se mueve una casilla hacia abajo
+                resultado = true;
+            }
+        }
         return resultado;
     }
 
