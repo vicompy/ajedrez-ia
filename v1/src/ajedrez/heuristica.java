dbf3cf8a7038aa6c28dd9964fcf6b6d69b3fe253
williambl17 <williambl17@85f47791-3065-007b-8d44-3d2c4d769e46>
2011-05-26 05:21:06 +0000

    Metodo validaTorre desarrollado solo para movimientos horizontales, falta revisión y optimización

--- v1/src/ajedrez/heuristica.java
+++ v1/src/ajedrez/heuristica.java
@@ -49,11 +49,63 @@ public class heuristica {
         return resultado;
     }
 
-    public boolean validaTorre(int oX, int oY, int dX, int dY, int pieza){
-        boolean resultado = false;
+    public boolean validaTorre(int oX, int oY, int dX, int dY, int[][] tablero){
+        boolean resultado = true;
+        int pieza = tablero[oX][oY];
         if(pieza > 0)
         {
             //es una pieza blanca
+            if(oX != dX && oY == dY){
+                //se mueve horizontalmente
+                if(dX > oX){
+                    //desplazamiento hacia abajo
+                    for(int i = oX + 1; i < dX; i++){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                    //verificar si la casilla destino esta ocupada
+                    if(tablero[dX][dY] == 10){
+                        //la casilla esta vacia
+                        resultado = resultado && true;
+                    }
+                    else if(tablero[dX][dY] < 0){
+                        //la casilla tiene una pieza oponente
+                        resultado = resultado && true;
+                    }
+                    else{
+                        //la casilla esta ocupada por una pieza propia
+                        resultado = false;
+                    }
+                }
+                else{
+                    //desplazamiento hacia arriba
+                    for(int i = oX - 1; i > dX; i--){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                    //verificar si la casilla destino esta ocupada
+                    if(tablero[dX][dY] == 10){
+                        //la casilla esta vacia
+                        resultado = resultado && true;
+                    }
+                    else if(tablero[dX][dY] < 0){
+                        //la casilla tiene una pieza oponente
+                        resultado = resultado && true;
+                    }
+                    else{
+                        //la casilla esta ocupada por una pieza propia
+                        resultado = false;
+                    }
+                }
+            }
+            else if(oX == dX && oY != dY){
+                //se mueve verticalmente
+            }
+            
             if(dX == oX - 1 && dY == oY){
                 //se mueve una casilla hacia arriba
                 resultado = true;
