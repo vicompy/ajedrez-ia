d8384671372b9b54e1fe498dcdae1154c5b71fef
williambl17 <williambl17@85f47791-3065-007b-8d44-3d2c4d769e46>
2011-05-26 19:48:55 +0000

    Metodo validaTorre finalizado

--- v1/src/ajedrez/heuristica.java
+++ v1/src/ajedrez/heuristica.java
@@ -63,11 +63,11 @@ public class heuristica {
                 //se mueve una casilla hacia abajo si esta vacia
                 resultado = true;
             }
-            else if(dX == oX + 1 && dY == oY - 1 && tablero[dX][dY] > 0){
+            else if(dX == oX + 1 && dY == oY - 1 && tablero[dX][dY] > 0 && tablero[dX][dY] != 10){
                 //se mueve en diagonal a la izquierda si hay una pieza del oponente
                 resultado = true;
             }
-            else if(dX == oX + 1 && dY == oY + 1 && tablero[dX][dY] > 0){
+            else if(dX == oX + 1 && dY == oY + 1 && tablero[dX][dY] > 0 && tablero[dX][dY] != 10){
                 //se mueve en diagonal a la derecha si hay una pieza del oponente
                 resultado = true;
             }
@@ -77,122 +77,128 @@ public class heuristica {
 
     //public boolean validaTorre(int oX, int oY, int dX, int dY, int[][] tablero){
      public boolean validaTorre(){
-        boolean resultado = true;
+        boolean resultado = false;
         int pieza = tablero[oX][oY];
         if(pieza > 0)
         {
             //es una pieza blanca
             if(oX != dX && oY == dY){
-                //se mueve horizontalmente
-                if(dX > oX){
-                    //desplazamiento hacia abajo
-                    for(int i = oX + 1; i < dX; i++){
-                        //verificar si las casillas intermedias estan libres
-                        if(tablero[i][dY] != 10){
-                            resultado = false;
-                        }
-                    }
-                    //verificar si la casilla destino esta ocupada
-                    if(tablero[dX][dY] == 10){
-                        //la casilla esta vacia
-                        resultado = resultado && true;
-                    }
-                    else if(tablero[dX][dY] < 0){
-                        //la casilla tiene una pieza oponente
-                        resultado = resultado && true;
-                    }
-                    else{
-                        //la casilla esta ocupada por una pieza propia
-                        resultado = false;
-                    }
-                }
-                else{
-                    //desplazamiento hacia arriba
-                    for(int i = oX - 1; i > dX; i--){
-                        //verificar si las casillas intermedias estan libres
-                        if(tablero[i][dY] != 10){
-                            resultado = false;
-                        }
-                    }
-                    //verificar si la casilla destino esta ocupada
-                    if(tablero[dX][dY] == 10){
-                        //la casilla esta vacia
-                        resultado = resultado && true;
-                    }
-                    else if(tablero[dX][dY] < 0){
-                        //la casilla tiene una pieza oponente
-                        resultado = resultado && true;
-                    }
-                    else{
-                        //la casilla esta ocupada por una pieza propia
-                        resultado = false;
-                    }
-                }
-            }
-            else if(oX == dX && oY != dY){
                 //se mueve verticalmente
-            }
-            if(oX != dX && oY == dY){
-                //se mueve horizontalmente
-                if(dX > oX){
-                    //desplazamiento hacia abajo
-                    for(int i = oX + 1; i < dX; i++){
-                        //verificar si las casillas intermedias estan libres
-                        if(tablero[i][dY] != 10){
-                            resultado = false;
-                        }
-                    }
-                    //verificar si la casilla destino esta ocupada
-                    if(tablero[dX][dY] == 10){
-                        //la casilla esta vacia
-                        resultado = resultado && true;
-                    }
-                    else if(tablero[dX][dY] < 0){
-                        //la casilla tiene una pieza oponente
-                        resultado = resultado && true;
-                    }
-                    else{
-                        //la casilla esta ocupada por una pieza propia
-                        resultado = false;
-                    }
-                }
-                else{
-                    //desplazamiento hacia arriba
-                    for(int i = oX - 1; i > dX; i--){
-                        //verificar si las casillas intermedias estan libres
-                        if(tablero[i][dY] != 10){
-                            resultado = false;
-                        }
-                    }
-                    //verificar si la casilla destino esta ocupada
-                    if(tablero[dX][dY] == 10){
-                        //la casilla esta vacia
-                        resultado = resultado && true;
-                    }
-                    else if(tablero[dX][dY] < 0){
-                        //la casilla tiene una pieza oponente
-                        resultado = resultado && true;
-                    }
-                    else{
-                        //la casilla esta ocupada por una pieza propia
-                        resultado = false;
-                    }
-                }
-            }
-            else if(oX == dX && oY != dY){
-                //se mueve verticalmente
-            }
-            if(dX == oX - 1 && dY == oY){
-                //se mueve una casilla hacia arriba
                 resultado = true;
+                if(dX > oX){
+                    //desplazamiento hacia abajo
+                    for(int i = oX + 1; i < dX; i++){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+                else{
+                    //desplazamiento hacia arriba
+                    for(int i = oX - 1; i > dX; i--){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+            }
+            else if(oX == dX && oY != dY){
+                //se mueve horizontalmente
+                resultado = true;
+                if(dY > oY){
+                    //desplazamiento hacia derecha
+                    for(int i = oY + 1; i < dY; i++){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[dX][i] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+                else{
+                    //desplazamiento hacia izquierda
+                    for(int i = oY - 1; i > dY; i--){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[dX][i] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+            }
+            //verificar si la casilla destino esta ocupada
+            if(tablero[dX][dY] == 10){
+                //la casilla esta vacia
+                resultado = resultado && true;
+            }
+            else if(tablero[dX][dY] < 0){
+                //la casilla tiene una pieza oponente
+                resultado = resultado && true;
+            }
+            else{
+                //la casilla esta ocupada por una pieza propia
+                resultado = false;
             }
         }
         else
         {
             //es una pieza negra
-            if(dX == oX + 1 && dY == oY){
-                //se mueve una casilla hacia abajo
+            if(oX != dX && oY == dY){
+                //se mueve verticalmente
                 resultado = true;
+                if(dX > oX){
+                    //desplazamiento hacia abajo
+                    for(int i = oX + 1; i < dX; i++){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+                else{
+                    //desplazamiento hacia arriba
+                    for(int i = oX - 1; i > dX; i--){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[i][dY] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+            }
+            else if(oX == dX && oY != dY){
+                //se mueve horizontalmente
+                resultado = true;
+                if(dY > oY){
+                    //desplazamiento hacia derecha
+                    for(int i = oY + 1; i < dY; i++){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[dX][i] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+                else{
+                    //desplazamiento hacia izquierda
+                    for(int i = oY - 1; i > dY; i--){
+                        //verificar si las casillas intermedias estan libres
+                        if(tablero[dX][i] != 10){
+                            resultado = false;
+                        }
+                    }
+                }
+            }
+            //verificar si la casilla destino esta ocupada
+            if(tablero[dX][dY] == 10){
+                //la casilla esta vacia
+                resultado = resultado && true;
+            }
+            else if(tablero[dX][dY] > 0 && tablero[dX][dY] != 10){
+                //la casilla tiene una pieza oponente
+                resultado = resultado && true;
+            }
+            else{
+                //la casilla esta ocupada por una pieza propia
+                resultado = false;
             }
         }
         return resultado;
